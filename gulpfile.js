bff5088a370b3f868eed4f59080cabf9d5b8a3d8
Tamas Csizmadia <tamas@me.com>
2016-08-28 14:03:45 +0200

    Nagyon primitív geoJSON-támogatás a gulpfile-ban. Egyelőre csak a stops.txt készül el a geojson mappában a `gulp geojson` parancs futtatásával.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ var packageName = 'ktrtfeed.zip';
 
 var gulp = require('gulp'),
   zip = require('gulp-zip'),
-  exec = require('child_process').execFile;
+  exec = require('child_process').execFile,
+  fileProcess = require('gulp-file-process'),
+  gtfs2geojson = require('gtfs2geojson'),
+  fs = require('fs');
 
 gulp.task('validate', function(cb) {
   exec("python", ['validator/feedvalidator.py', '-n', '-o', 'validator/report.html', 'feed/'], function(err, stdout, stderr) {
@@ -19,6 +22,16 @@ gulp.task('package', ['validate'], function() {
     return gulp.src('feed/*.txt')
       .pipe(zip(packageName))
       .pipe(gulp.dest('dist'));
+});
+
+gulp.task('geojson', function(cb) {
+  gulp.src('feed/stops.txt')
+    .pipe(fileProcess({
+      process: function(file, content) {
+        var result = gtfs2geojson.stops(content);
+        fs.writeFileSync('geojson/stops.geojson', JSON.stringify(result, null, 2));
+      }
+    }));
 });
 
 gulp.task('default', function() {
